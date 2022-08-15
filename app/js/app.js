a9d3999cab1e5e7a9fa4202f510bde8d3606365e
sverben <59171289+sverben@users.noreply.github.com>
2022-08-15 21:49:10 +0200

    Add key descriptions

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,7 @@ const title = document.getElementById("title");
 const colorWidget = document.getElementById("colorWidget");
 const color = document.getElementById("color");
 const gameTitle = document.getElementById("gameTitle");
+const keys = document.getElementById("keys");
 
 export default class App {
     constructor(game, zoomed) {
@@ -13,6 +14,7 @@ export default class App {
 
         this.keys = {};
         this.game.keys.forEach(key => this.keys[key] = false);
+        this.setKeys();
 
         this.setupColor();
 
@@ -31,6 +33,48 @@ export default class App {
             this.socket.emit("color", color.value);
             localStorage.setItem("lastColor", color.value);
         });
+    }
+
+    setKeys() {
+        keys.innerHTML = "<h3>Keys</h3>";
+        for (const explanation of this.game.explanation) {
+            const line = document.createElement("div");
+            line.classList.add("line");
+            switch (explanation.type) {
+                case "grid": {
+                    const layout = document.createElement("div");
+                    layout.classList.add("grid");
+                    for (const row of explanation.layout) {
+                        const rowE = document.createElement("div");
+                        rowE.classList.add("row");
+                        for (const char of row) {
+                            const e = document.createElement("div");
+                            if (char !== null) {
+                                e.classList.add("key");
+                                e.innerText = char.toUpperCase();
+                            }
+                            rowE.append(e);
+                        }
+                        layout.append(rowE);
+                    }
+                    line.append(layout);
+                    break;
+                } case "large": {
+                    const e = document.createElement("div");
+                    e.classList.add("large");
+                    e.innerText = explanation.text;
+
+                    line.append(e);
+                    break;
+                }
+            }
+            const name = document.createElement("div");
+            name.classList.add("description");
+            name.innerText = explanation.description;
+            line.append(name);
+
+            keys.append(line);
+        }
     }
 
     setupColor() {
